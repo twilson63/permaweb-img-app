80adf496d358cfcc6fd19b7063efef78416ef67d
Tom Wilson <tom@hyper.io>
2022-09-03 16:50:46 +0000

    upload file

--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,3 +1,27 @@
+<script>
+  import { deploy } from "../lib/deploy.js";
+  let files = [];
+
+  const toArrayBuffer = (file) =>
+    new Promise((resolve, reject) => {
+      const fr = new FileReader();
+      fr.readAsArrayBuffer(file);
+      fr.addEventListener("loadend", (evt) => {
+        resolve(evt.target.result);
+      });
+    });
+
+  async function doDeploy() {
+    // connnect
+    await arweaveWallet.connect(["ACCESS_ADDRESS", "SIGN_TRANSACTION"]);
+    const addr = await arweaveWallet.getActiveAddress();
+    const data = await toArrayBuffer(files[0]);
+    const result = await deploy("TEST", addr, "image/png", data);
+
+    console.log(result);
+  }
+</script>
+
 <nav
   class="flex space-x-4 h-[75px] bg-secondary text-secondary-content flex items-center"
 >
@@ -7,8 +31,17 @@
 <main>
   <section class="hero min-h-screen bg-base-100">
     <div class="hero-content flex-col">
-      <h1 class="text-8xl font-bold">⚡️HypAR⚡️</h1>
-      <p>The Ultimate Permaweb Stack!</p>
+      <h1 class="text-8xl font-bold">img</h1>
+      <p>Upload</p>
+      <form class="form" on:submit|preventDefault={doDeploy}>
+        <div class="form-control mb-16">
+          <label for="file" class="label">Choose Image</label>
+          <input id="file" type="file" bind:files />
+        </div>
+        <div class="">
+          <button class="btn btn-block">Deploy</button>
+        </div>
+      </form>
     </div>
   </section>
 </main>
